1f2819f5794f62049357594a29b2e318b1018a85
Tina Shi <init.defaultBranch>
2023-03-17 22:38:56 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+/*function saturdayFun(activity = 'roller-skate') {
+    console.log(`'This Saturday, I want to ${activity}!'`);
+}
+saturdayFun();
+saturdayFun('bathe my dog'); */
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(sym = "*") {
+    const innerFunction = function (adj = "special") {
+        return `You are ${sym}${adj}${sym}!`;
+    };
+    return innerFunction;
+}
+
